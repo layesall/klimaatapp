657d8abb25b4bef8c37f59f0aa5145321d1e200f
Layesall <contact@layesall.com>
2022-07-18 10:05:52 +0200

    add: hooks folder | useWeather

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,10 @@
-
-import { FontAwesome } from '@expo/vector-icons'
+import { API_KEY } from "../constants/api-key";
+import { FontAwesome } from "@expo/vector-icons";
 
 export const getIcon = (name, color, size) => {
-  return (
-    <FontAwesome name={name} color={color} size={size} />
-  )
-}
+  return <FontAwesome name={name} color={color} size={size} />;
+};
+
+export const getApiUrl = (cityName) => {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric&lang=fr`;
+};
