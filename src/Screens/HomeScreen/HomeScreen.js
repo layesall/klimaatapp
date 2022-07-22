cc9d9b5bde25c4ce7c0c35148e837db7f58f26c6
Layesall <contact@layesall.com>
2022-07-22 20:31:12 +0200

    edit: home-screen have just the form-component

--- src/Screens/HomeScreen/HomeScreen.js
+++ src/Screens/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import {SafeAreaView} from "react-native";
 import FormComponent from "../../components/FormComponent/FormComponent";
 
 import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
